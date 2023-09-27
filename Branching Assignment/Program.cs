0a606216a2abe200e0aea320ead8dc09a779844e
aziza <aziza@DESKTOP-O5LFJAP>
2023-09-27 13:39:00 -0400

    Add project files.

---
+++ Branching Assignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display the welcome message
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            // Prompt the user for package weight
+            Console.WriteLine("Please enter the package weight:");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            // Check if weight is greater than 50
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                // End the program
+                return;
+            }
+
+            // Prompt the user for package width
+            Console.WriteLine("Please enter the package width:");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            // Prompt the user for package height
+            Console.WriteLine("Please enter the package height:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            // Prompt the user for package length
+            Console.WriteLine("Please enter the package length:");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            // Calculate the total dimensions
+            double totalDimensions = width + height + length;
+
+            // Check if total dimensions are greater than 50
+            if (totalDimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                // End the program
+                return;
+            }
+
+            // Calculate the quote
+            double quote = (width * height * length * weight) / 100;
+
+            // Display the quote to the user
+            Console.WriteLine($"Your estimated total for shipping this package is: ${quote:F2}");
+            Console.WriteLine("Thank you!");
+        }
+    }
+}
